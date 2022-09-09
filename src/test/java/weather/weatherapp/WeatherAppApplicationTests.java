871e9684bfb4490e4ecd58095429059469a8c8d5
Rahman <rahmanrejepov777@gmail.com>
2022-09-09 15:01:27 +0300

    Initial commit

---
+++ src/test/java/weather/weatherapp/WeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package weather.weatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
