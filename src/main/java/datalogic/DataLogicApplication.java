53345c9eff912cfbb38d2a3432ff44396885be9e
Rahman <rahmanrejepov777@gmail.com>
2022-10-03 15:26:54 +0300

    The start of the project by configuring Flyway

---
+++ src/main/java/datalogic/DataLogicApplication.java
@@ -0,0 +1,13 @@
+package datalogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataLogicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/datalogic/DataLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package datalogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
